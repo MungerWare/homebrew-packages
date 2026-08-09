8875395967f84602c0adc605d18ae593a7a61cd7
Munger <munger@mungerware.com>
2026-08-10 00:44:18 +0100

    ocsessnotify 1.0.3: async prompt post, cross-project target

--- Formula/ocsessnotify.rb
+++ Formula/ocsessnotify.rb
@@ -2,9 +2,9 @@
 class Ocsessnotify < Formula
   desc "Post a message to the most recent opencode session on the LAN"
   homepage "https://github.com/Munger/InfraTools"
-  url "https://raw.githubusercontent.com/Munger/InfraTools/ocsessnotify-v1.0.2/ocsessnotify/usr/local/bin/ocsessnotify"
-  version "1.0.2"
-  sha256 "65a26c172180279c1039113af14e19dd63c731ef3248f04a07eaee73396a7ea0"
+  url "https://raw.githubusercontent.com/Munger/InfraTools/ocsessnotify-v1.0.3/ocsessnotify/usr/local/bin/ocsessnotify"
+  version "1.0.3"
+  sha256 "6ed58e9b8440e6badb5faeaed342572b2eca94fdb51626de002b2b9c1f9a7e2d"
   license "MIT"
 
   depends_on "jq"
